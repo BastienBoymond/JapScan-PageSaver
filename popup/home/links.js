28aee2025fb48607ac89edd087addff7ac9ee605
BastienBoymond <71839723+BastienBoymond@users.noreply.github.com>
2021-08-30 23:19:25 +0200

    start of a beautiful popup for have last manga and delete the last

---
+++ popup/home/links.js
@@ -0,0 +1,12 @@
+window.onclick = function(event) {
+    const target = event.target;
+    if (target.matches('.news-buttons')) {
+        window.location.href = '../news/news.html'
+    } else if (target.matches('.github-buttons')) {
+        window.open('https://github.com/BastienBoymond/JapScan-PageSaver');
+    } else if (target.matches('.resume-buttons')) {
+        window.location.href = '../resume/resume.html'
+    } else if (target.matches('.delete-buttons')) {
+        window.location.href = '../delete/delete.html'
+    }
+}
