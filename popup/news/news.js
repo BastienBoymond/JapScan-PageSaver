d22e4585a3d5d5922c5b9f558f1246dba5e7f435
Bastien <bastien.boymond@epitech.eu>
2022-10-20 13:34:18 +0200

    add of searchbar in menus

--- popup/news/news.js
+++ popup/news/news.js
@@ -87,3 +87,18 @@ window.onclick = async function(event) {
         window.location.href = '../home/popup.html'
     }
 }
+
+window.addEventListener('input', async (event) => {
+    const searchContent = event.target.value;
+    console.log(searchContent);
+    document.querySelectorAll('button').forEach((button) => {
+        if (button.innerText.includes(searchContent)) {
+            button.style.display = 'inline-block';
+        } else if (button.innerText.replaceAll('-', ' ').includes(searchContent)) {
+            button.style.display = 'inline-block';
+        } else {
+            if (button.className === 'goBack') return;
+            button.style.display = 'none';
+        }
+    });
+});
