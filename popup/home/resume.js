2cf366eb2c920f113a073780fc14e1e52be7e894
Bastien <bastien.boymond@epitech.eu>
2022-11-07 17:36:58 +0100

    start of rework button

--- popup/home/resume.js
+++ popup/home/resume.js
@@ -6,10 +6,13 @@ async function createButtonResumes() {
     if (mangaList) {
         mangaList.map(async (manga) => {
             const resume = await get_stored_value(manga);
-            const button = document.createElement('button');
+            const button = document.createElement('div');
+            button.className = manga + ' manga-button';
             const chOrVol = resume.type === 'volume' ? 'Vol' : 'Ch';
-            button.innerText = manga + '\n' + chOrVol +resume.chapter + ' - ' + 'p' +resume.page;
-            button.className = manga;
+            button.innerHTML =  `<img src="https://www.japscan.me/imgs/mangas/${manga}.jpg" class="${manga} manga-image">
+                                <div class="${manga} manga-title">${manga}</div>
+                                <div class="${manga} manga-chapter">${chOrVol + resume.chapter}</div>
+                                <div class="${manga} manga-page">p${resume.page}</div>`;
             document.getElementsByClassName('resume-content')[0].appendChild(button);
         });
     }
@@ -20,8 +23,12 @@ createButtonResumes();
 window.onclick = async function(event) {
     const target = event.target;
     if (target.className !== 'scroll-bar resume-content' && target.className !== 'goBack' && target.className !== 'fas fa-arrow-left') {
-        const manga = target.className;
+        console.log(target.className);
+        if (!target.className.includes('manga'))
+            return;
+        const manga = target.className.replace(' manga-button', '').replace(' manga-image', '').replace(' manga-title', '').replace(' manga-chapter', '').replace(' manga-page', '');
         const resume = await get_stored_value(manga);
+        if (!resume) return;
         const type = resume.type === 'volume' ? 'volume-' : '';
         window.open(`https://japscan.com/lecture-en-ligne/${manga}/${type}${resume.chapter}/${resume.page}.html`);
     } else if (target.className === "goBack" || target.className === "fas fa-arrow-left") {
