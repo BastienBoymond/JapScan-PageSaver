da74768a403a4e5ea44740d49cdb84f7467869ae
Bastien Boymond <71839723+BastienBoymond@users.noreply.github.com>
2023-02-09 20:41:22 +0100

    need to do back

--- website/script.js
+++ website/script.js
@@ -1,14 +1,20 @@
 import {loadtheme} from '../module/theming.js';
 import { get_stored_value, store_value } from '../module/storage.js';
-import { requestGet } from '../module/request.js';
+import { requestGet, requestPost } from '../module/request.js';
 
 function checkIfLoggedIn() {
     const token = get_stored_value("token_stats");
-    if (!token) return;
+    if (!token) return false;
     // Faire la requête pour vérifier si le token est valide
     // Si oui, afficher changer de page directement vers la page de stats
     // Sinon, rester sur cette page et attendre une action de l'utilisateur
 }
 
+
+
 loadtheme();
-checkIfLoggedIn();
+if (checkIfLoggedIn())  {
+    // Afficher la page de stats
+} else {
+    // load List of manga in menu
+}
