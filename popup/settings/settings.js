f8c458b8d79459714be642475aa635b9e7340bc4
Bastien. B <Bastien.boymond@epitech.eu>
2023-06-25 14:24:52 +0200

    Feature-7 implement of import export for bugged user (#20)

--- popup/settings/settings.js
+++ popup/settings/settings.js
@@ -25,8 +25,41 @@ window.onclick = async function(event) {
                 store_value('japscan_auto_anilist', true);
             }
         }
+    } else if (event.target.matches('.export-list')) {
+        const list_name = await get_stored_value('japscan_manga_name');
+        const list_progress = {};
+
+        for (const manga of list_name) {
+            const progress = await get_stored_value(manga);
+            list_progress[manga] = progress;
+        }
+        const format_both = {list_name: list_name, list_progress: list_progress}
+        const blob = new Blob([JSON.stringify(format_both)], {type: 'application/json'});
+        const url = URL.createObjectURL(blob);
+        chrome.downloads.download({
+            url: url,
+            filename: 'japscan_list.json'
+        });
+    } else if (event.target.matches('.import-list')) {
+        document.querySelector('input[type=file]').click();
+
+        document.querySelector('input[type=file]').onchange = async function() {
+            const file = this.files[0];
+            const reader = new FileReader();
+            reader.onload = async function(progressEvent) {
+                const list = JSON.parse(this.result);
+                const list_name = list.list_name;
+                const list_progress = list.list_progress;
+
+                for (const manga of list_name) {
+                    const progress = list_progress[manga];
+                    store_value(manga, progress);
+                }
+                store_value('japscan_manga_name', list_name);
+            };
+            reader.readAsText(file);
+        }
     }
-    
     if (event.target.matches('.website')) {
         let internalUrl = chrome.runtime.getURL("website/index.html");
         chrome.tabs.create({url: internalUrl});
